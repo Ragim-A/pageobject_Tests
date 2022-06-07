3e823757298ad49caab774c38ecfb0a8cbc3e332
Ragim <ratutnetu@gmail.com>
2022-06-07 18:10:57 +0300

    Добавил файл с тестом на форму

---
+++ src/test/java/ru/nxt/rmail/RegistrationFormTest.java
@@ -0,0 +1,46 @@
+package ru.nxt.rmail;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormTest {
+
+    @Test
+    void successfulTest() {
+        String firstName = "Vova";
+        String lastName = "Kotov";
+        String email = "vova@kotov.com";
+        String Tel = "89103133333";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue(Tel);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").sendKeys("Maths");
+        $("#subjectsInput").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.jpeg");
+        $("#currentAddress").setValue("Some street 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(email), text("Some street 1"), text("30 May,2000"));
+    }
+}
