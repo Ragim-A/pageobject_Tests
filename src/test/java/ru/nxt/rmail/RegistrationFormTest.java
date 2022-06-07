8c78e0959483e09e9fc9b57bfa70cf650aca4871
Ragim <ratutnetu@gmail.com>
2022-06-07 23:01:10 +0300

    ДЗ занятие 3

--- src/test/java/ru/nxt/rmail/RegistrationFormTest.java
+++ src/test/java/ru/nxt/rmail/RegistrationFormTest.java
@@ -1,19 +1,23 @@
 package ru.nxt.rmail;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistrationFormTest {
+
+public class RegistrationFormTest extends TestBase{
+
 
     @Test
     void successfulTest() {
         String firstName = "Vova";
         String lastName = "Kotov";
         String email = "vova@kotov.com";
-        String Tel = "89103133333";
+        String Tel = "8910313333";
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -32,7 +36,7 @@ public class RegistrationFormTest {
         $("#subjectsInput").sendKeys("Maths");
         $("#subjectsInput").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("img/1.jpeg");
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue("Some street 1");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
@@ -41,6 +45,8 @@ public class RegistrationFormTest {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(email), text("Some street 1"), text("30 May,2000"));
-    }
-}
+
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(email), text(Tel),
+                text("Male"), text("Maths"), text("Sports"), text("Some street 1"),
+                text("30 May,2000"), text ("NCR"), text("Delhi"));
+}}
