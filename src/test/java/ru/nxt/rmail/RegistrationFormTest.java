9913ca66087e585055f86a5fbfdc15f744372252
Ragim <ratutnetu@gmail.com>
2022-07-23 19:40:23 +0300

    Jenkins+allure code added

--- src/test/java/ru/nxt/rmail/RegistrationFormTest.java
+++ src/test/java/ru/nxt/rmail/RegistrationFormTest.java
@@ -1,40 +1,47 @@
 package ru.nxt.rmail;
 
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 import static ru.nxt.rmail.TestData.*;
 
-public class RegistrationFormTest extends TestBase{
+public class RegistrationFormTest extends TestBase {
     @Test
     void successfulTest() {
 
-//Заполнение формы
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setTel(tel)
-                .setSubjects(subject)
-                .setAdress(adress)
-                .setGender(gender)
-                .setState(state)
-                .setCity(city)
-                .setDateOfBirth(date,month,year)
-                .setFilename(fileName)
-                .setHobbie(hobbie)
-                .clickSubmit();
+        step("Open registrations form", () -> {
+            registrationFormPage.openPage();
+        });
+        step("Fill form", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setTel(tel)
+                    .setSubjects(subject)
+                    .setAdress(adress)
+                    .setGender(gender)
+                    .setState(state)
+                    .setCity(city)
+                    .setDateOfBirth(date, month, year)
+                    .setFilename(fileName)
+                    .setHobbie(hobbie)
+                    .clickSubmit();
+        });
 
-//Проверка таблицы с результатами
-        registrationFormPage
-                .checkSubmitting()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", email)
-                .checkResult("Mobile",tel)
-                .checkResult("Gender",gender)
-                .checkResult("Date of Birth",date+" "+month+","+year)
-                .checkResult("Subjects", subject)
-                .checkResult("Hobbies", hobbie)
-                .checkResult("Picture",fileName)
-                .checkResult("Address", adress)
-                .checkResult("State and City",state+" "+city);
+        step("Check results", () -> {
+            registrationFormPage
+                    .checkSubmitting()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Mobile", tel)
+                    .checkResult("Gender", gender)
+                    .checkResult("Date of Birth", date + " " + month + "," + year)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", hobbie)
+                    .checkResult("Picture", fileName)
+                    .checkResult("Address", adress)
+                    .checkResult("State and City", state + " " + city);
 
-    }}
+        });
+    }
+}
